22187d1ac45df440009795c6e596f513a40e01a5
byrn-baker <byrnbaker@icloud.com>
2021-05-15 23:01:18 -0600

    update

--- Python_Scripts/nautobot_load.py
+++ Python_Scripts/nautobot_load.py
@@ -9,7 +9,7 @@ data_file = "nb_initial_load.yaml"
 with open(data_file) as f: 
     data = yaml.safe_load(f.read())
 
-nb = api(url="http://192.168.130.2:8000", token="c7fdc6be609a244bb1e851c5e47b3ccd9d990b58")
+nb = api(url="http://192.168.130.202:8000", token="c7fdc6be609a244bb1e851c5e47b3ccd9d990b58")
 nb.http_session.verify = False
 
 # sites: 
@@ -17,7 +17,45 @@ for site in data["sites"]:
     print(f"Creating or Updating Site {site['name']}")
     nb_data = nb.dcim.sites.get(slug=site["slug"])
     if not nb_data: 
-        nb_data = nb.dcim.sites.create(name=site["name"], slug=site["slug"])
+        nb_data = nb.dcim.sites.create(
+            name=site["name"],
+            slug=site["slug"],
+            status=site["status"],
+        )
+    nb_site = nb.dcim.sites.get(slug=site["slug"])
+    if "asn" in site.keys():        
+        nb_site.asn = site["asn"]
+    if "time_zone" in site.keys():    
+        nb_site.time_zone = site["time_zone"]
+    if "description" in site.keys():    
+        nb_site.description = site["description"]
+    if "physical_address" in site.keys():
+        nb_site.physical_address = site["physical_address"]
+    if "shipping_address" in site.keys():
+        nb_site.shipping_address = site["shipping_address"]
+    if "latitude" in site.keys():
+        nb_site.latitude = site["latitude"]
+    if "longitude" in site.keys():
+        nb_site.longitude = site["longitude"]
+    if "contact_name" in site.keys():
+        nb_site.contact_name = site["contact_name"]
+    if "contact_phone" in site.keys():
+        nb_site.contact_phone = site["contact_phone"]
+    if "contact_email" in site.keys():
+        nb_site.contact_email = site["contact_email"]
+    if "comments" in site.keys():
+        nb_site.comments = site["comments"]
+    nb_site.save()
+# Relay Racks
+    for rack in site["racks"]:
+        print(f"Creating or Updating Relay rack {rack['name']} for site {site['name']}")
+        nb_rack = nb.dcim.racks.get(site=site["slug"])
+        if not nb_rack:
+            nb_rack = nb.dcim.racks.create(
+                name=rack["name"],
+                status=rack["status"],
+                site=nb.dcim.sites.get(slug=site["slug"]).id
+            )      
 
 # manufacturers 
 for manufacturer in data["manufacturers"]: 
@@ -32,10 +70,10 @@ for device_type in data["device_types"]:
     nb_data = nb.dcim.device_types.get(slug=device_type["slug"])
     if not nb_data: 
         nb_data = nb.dcim.device_types.create(
-            model=device_type["model"], 
-            slug=device_type["slug"], 
-            manufacturer=nb.dcim.manufacturers.get(slug=device_type["manufacturer_slug"]).id, 
-            height=device_type["height"]
+            model = device_type["model"], 
+            slug = device_type["slug"], 
+            manufacturer = nb.dcim.manufacturers.get(slug=device_type["manufacturer_slug"]).id, 
+            height = device_type["height"]
             )
 
 # device_roles
@@ -95,16 +133,17 @@ for vrf in data["vrfs"]:
 
 # vlans
 for vlan in data["vlans"]: 
-    print(f"Creating or updating vlan {vlan['name']}")
+    print(f"Creating or updating vlan {vlan['name']} at site {vlan['site']}")
     nb_vlan = nb.ipam.vlans.get(
         site=vlan["site"],
         vid=vlan["vid"],
     )
     if not nb_vlan: 
         nb_vlan = nb.ipam.vlans.create(
-            site=vlan["site"], 
+            site=nb.dcim.sites.get(slug=vlan["site"]).id, 
             name=vlan["name"], 
-            vid=vlan["vid"],  
+            vid=vlan["vid"],
+            status=vlan["status"] 
         )
     if "prefix" in vlan.keys(): 
         print(f"Configuring prefix {vlan['prefix']}")
@@ -133,7 +172,7 @@ for pfx in data["prefixes"]:
             prefix=pfx["prefix"],
             site={"slug": pfx["site"]},
             description=pfx["description"],
-            status="active",
+            status=pfx["status"],
         )
 
 # devices
@@ -146,10 +185,21 @@ for device in data["devices"]:
             manufacturer=nb.dcim.manufacturers.get(slug=device["manufacturer_slug"]).id, 
             site=nb.dcim.sites.get(slug=device["site_slug"]).id,
             device_role=nb.dcim.device_roles.get(slug=device["device_role_slug"]).id, 
-            device_type=nb.dcim.device_types.get(slug=device["device_types_slug"]).id, 
+            device_type=nb.dcim.device_types.get(slug=device["device_types_slug"]).id,
+            status=device["status"],
             )
+    if nb_device.rack is None:
+        print(f"Moving device into rack {device['rack']}")
+        if "rack" in device.keys():
+            nb_device.rack = nb.dcim.racks.get(site=device["site_slug"]).id
+        if "position" in device.keys():
+            nb_device.position = device["position"]
+        if "face" in device.keys():    
+            nb_device.face = device["face"]
+        nb_device.save()
+
     for interface in device["interfaces"]: 
-        print(f"  Creating or updating interface {interface['name']}")
+        print(f"Creating or updating interface {interface['name']} on device {device['name']}")
         nb_interface = nb.dcim.interfaces.get(
             device_id=nb_device.id, 
             name=interface["name"]
@@ -157,12 +207,11 @@ for device in data["devices"]:
         if not nb_interface: 
             nb_interface = nb.dcim.interfaces.create(
                 device=nb_device.id, 
-                name=interface["name"], 
+                name=interface["name"],
+                type=interface["type"] 
             )
         if "description" in interface.keys():
             nb_interface.description = interface["description"]
-        if "type" in interface.keys():
-            nb_interface.type = interface["type"]
         if "label" in interface.keys():
             nb_interface.label = interface["label"]
         if "mtu" in interface.keys():
@@ -227,7 +276,7 @@ for device in data["devices"]:
         nb_interface = nb.dcim.interfaces.get(device_id=nb_device.id, name=interface["name"])
         if nb_interface["cable"] is None:
             if "bside_device" in interface.keys():
-                print(f"  Creating or updating interface connections {device['name']}")
+                print(f"  Creating or updating interface connections between {device['name']}-{interface['name']} and {interface['bside_device']}-{interface['bside_interface']}")
                 int_a = nb.dcim.interfaces.get(name=interface["name"], device=nb.dcim.devices.get(name=device["name"])).id
                 int_b = nb.dcim.interfaces.get(name=interface["bside_interface"], device=nb.dcim.devices.get(name=interface["bside_device"])).id
                 nb.dcim.cables.create(
